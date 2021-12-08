01d59bf104b21bac485dc05790e6cbb16bce396b
Karolína Vorlíčková <36192566+karolinavor@users.noreply.github.com>
2021-12-08 10:55:11 +0100

    files

---
+++ main.c
@@ -0,0 +1,77 @@
+/**
+* @file    main.c
+* @author  Karolina Vorlickova <vorlka00@upol.cz>
+* @date    1.12.2021
+* @brief   Projekt Matice
+*
+* Modul pro praci s maticemi
+**/
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include "matice.h"
+
+int main()
+{
+    /*
+    matice mat1, mat2, mat3, mat4, mat5, mat6;
+    mat1 = jednotkova(3, 4);
+    nastav_prvek(mat1, 0, 1, 2);
+    nastav_prvek(mat1, 0, 2, 3);
+    nastav_prvek(mat1, 0, 3, 4);
+    nastav_prvek(mat1, 1, 0, 5);
+    nastav_prvek(mat1, 2, 3, 1);
+    printf("Matice mat1: \n");
+    vypis(mat1);
+    printf("\n\n");
+    mat2 = transpozice(mat1);
+    printf("Transpozice matice mat1: \n");
+    vypis(mat2);
+    printf("\n\n");
+    mat3 = plus(mat1, mat1);
+    printf("Soucet matic mat1 + mat1: \n");
+    vypis(mat3);
+    printf("\n\n");
+    mat4 = nasobeni(mat1, 3);
+    printf("Matice mat1 vynasobena 3: \n");
+    vypis(mat4);
+    printf("\n\n");
+    mat5 = krat(mat1, mat2);
+    printf("Matice mat1 vynasobena transponovanou matici mat1: \n");
+    switch (chyba)
+    {
+    case CHYBA_TYPU:
+        printf("S maticemi neni mozne provest nasobeni.");
+        break;
+    case CHYBA_ALOKACE:
+        printf("Nastala chyba alokace pameti.");
+        break;
+    case BEZ_CHYBY:
+        vypis(mat5);
+        break;
+        printf("\n\n");
+        mat6 = krat(mat1, mat1);
+        printf("Matice mat1 vynasobena matici mat1: \n");
+        switch (chyba)
+        {
+        case CHYBA_TYPU:
+            printf("S maticemi neni mozne provest nasobeni.");
+            break;
+        case CHYBA_ALOKACE:
+            printf("Nastala chyba alokace pameti.");
+            break;
+        case BEZ_CHYBY:
+            vypis(mat6);
+            break;
+        }
+    }
+    printf("\n\n");
+    odstran(mat1);
+    odstran(mat2);
+    odstran(mat3);
+    odstran(mat4);
+    odstran(mat5);
+    odstran(mat6);
+     */
+    return 0;
+}
--- matice.h
+++ matice.h
@@ -1,8 +1,36 @@
-/*
+/**
+* @file    matice.h
+* @author  Karolina Vorlickova <vorlka00@upol.cz>
+* @date    1.12.2021
+* @brief   Projekt Matice
+*
+* Modul pro praci s maticemi
+**/
 
-pouzivat float
-definovat strukturovany datovy typ matice
-velikost (int m a int n)
-Prvky matice budou ulozeny v dvourozmernym poli data.
+struct matrix
+{
+    /** @brief ID objektu */
+    int id;
+    /** @brief Souřadnice X */
+    int m;
+    /** @brief Souřadnice Y */
+    int n;
+};
 
+/**
+ * @brief  Inicializuje matici.
+ *
+ * @param c1 ukazatel na první objekt
+ * @param c2 ukazatel na druhý objekt
+ * @return vraci matici
 */
+float init_matrix(float c1, float c2);
+
+/**
+ * @brief Hlavní funkce programu, spouští se jako první.
+ * @param argc počet argumentů příkazové řádky
+ * @param argv pole argumentů příkazové řádky
+ *
+ * @return při úspěšném ukončení vrací 0, při neúspěšném -1
+ */
+int main(int argc, char *argv[]);
