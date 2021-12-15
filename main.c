16b0d63e0072553a3b83bcd72e4bd09899206776
Karolína Vorlíčková <36192566+karolinavor@users.noreply.github.com>
2021-12-15 09:38:09 +0100

    finished

--- main.c
+++ main.c
@@ -13,12 +13,9 @@
 
 int main()
 {
-    matice *mat1, *mat2, *mat3, *mat4, *mat5, *mat6, *mat7;
+    matice mat1, mat2, mat3, mat4, mat5, mat6, mat7;
 
     mat1 = jednotkova(3, 4);
-    if (!(kontrolaChyb() == BEZ_CHYBY)) {
-        exit(1);
-    }
 
     nastav_prvek(mat1, 0, 1, 2);
     nastav_prvek(mat1, 0, 2, 3);
@@ -26,71 +23,65 @@ int main()
     nastav_prvek(mat1, 1, 0, 5);
     nastav_prvek(mat1, 2, 3, 1);
 
-    if (!(kontrolaChyb() == BEZ_CHYBY)) {
-        exit(1);
-    }
-
     printf("Matice mat1: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat1);
-    } else {
-        exit(1);
-    }
+    vypis(mat1);
     printf("\n");
 
     printf("Velikost matice mat1 v dimenzi 0 (radky): \n");
     printf("%d",velikost(mat1, 0));
-    if (!(kontrolaChyb() == BEZ_CHYBY)) {
-        exit(1);
-    }
     printf("\n\n");
 
-    printf("Velikost matice mat1 v dimenzi 1(sloupce): \n");
+    printf("Velikost matice mat1 v dimenzi 1 (sloupce): \n");
     printf("%d",velikost(mat1, 1));
-    if (!(kontrolaChyb() == BEZ_CHYBY)) {
-        exit(1);
-    }
     printf("\n\n");
+
 
     printf("Prvek matice mat1 na pozici[0][1]: \n");
     printf("%f",prvek(mat1, 0, 1));
-    if (!(kontrolaChyb() == BEZ_CHYBY)) {
-        exit(1);
-    }
     printf("\n\n");
 
     mat2 = transpozice(mat1);
     printf("Transpozice matice mat1: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat2);
-    }
+    vypis(mat2);
     printf("\n\n");
 
     mat3 = plus(mat1, mat1);
     printf("Soucet matic mat1 + mat1: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat3);
-    }
+    vypis(mat3);
     printf("\n\n");
 
     mat4 = nasobeni(mat1, 3);
     printf("Matice mat1 vynasobena 3: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat4);
-    }
+    vypis(mat4);
     printf("\n\n");
 
     mat5 = krat(mat1, mat2);
     printf("Matice mat1 vynasobena transponovanou matici mat1: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat5);
+    switch (chyba) {
+        case CHYBA_TYPU:
+            printf("S maticemi neni mozne provest nasobeni.");
+            break;
+        case CHYBA_ALOKACE:
+            printf("Nastala chyba alokace pameti.");
+            break;
+        case BEZ_CHYBY:
+            vypis(mat5);
+            break;
     }
     printf("\n\n");
 
     mat6 = krat(mat1, mat1);
     printf("Matice mat1 vynasobena matici mat1: \n");
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat6);
+    switch (chyba) {
+        case CHYBA_TYPU:
+            printf("S maticemi neni mozne provest nasobeni.");
+            break;
+        case CHYBA_ALOKACE:
+            printf("Nastala chyba alokace pameti.");
+            break;
+        case BEZ_CHYBY:
+            vypis(mat6);
+            break;
     }
     printf("\n\n");
 
@@ -100,25 +91,19 @@ int main()
 
     printf("Zmena hodnoty prvku matice mat1 na pozici[0][1] na 10: \n");
     nastav_prvek(mat1, 0, 1, 10.0);
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat1);
-    }
+    vypis(mat1);
     printf("\n\n");
 
     printf("Matice nactena ze souboru soubor.txt: \n");
     char *soubor = "./soubor.txt";
     mat7 = nacti_ze_souboru(soubor);
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        vypis(mat7);
-    }
+    vypis(mat7);
     printf("\n\n");
 
     printf("Matice mat1 ulozena do souboru soubor2.txt\n");
     char *soubor2 = "./soubor2.txt";
     uloz_do_souboru(mat1,soubor2);
-    if (kontrolaChyb() == BEZ_CHYBY) {
-        printf("OK.");
-    }
+    printf("OK.");
     printf("\n");
 
     odstran(mat1);
--- matice.c
+++ matice.c
@@ -12,265 +12,355 @@
 #include "matice.h"
 
 // funkce vytvori matici typu m × n, vcetne alokovanı pole pro prvky matice
-matice* inicializace(int m, int n) {
+matice inicializace(int m, int n) {
+    matice nova_matice;
+
     // kontrola nulove a zaporne velikosti matice
     if (m <= 0 || n <= 0) {
         chyba = CHYBA_TYPU;
-        return NULL;
+        return nova_matice;
     }
 
-    // calloc = calloc sets allocated memory to zero
-    matice* nova_matice = calloc(1, sizeof(*nova_matice));
-    nova_matice->m = m;
-    nova_matice->n = n;
+    // dynamicka alokace pameti pro data
+    float **data;
 
-    // dynamicka alokace pameti
-    nova_matice->data = calloc(nova_matice->m, sizeof(*nova_matice->data));
-    for(int i = 0; i < nova_matice->m; ++i) {
-        nova_matice->data[i] = calloc(nova_matice->n, sizeof(**nova_matice->data));
-    }
-
-    // kontrola alokace
-    if (!nova_matice || !nova_matice->data)
-    {
-        odstran(nova_matice);
+    data = malloc(m * sizeof(float *));
+    if (!data) {
         chyba = CHYBA_ALOKACE;
-        return NULL;
+        return nova_matice;
     }
+
+    for (int i = 0; i < m; i++)
+    {
+        data[i] = (float *)malloc(n * sizeof(float));
+        if (!(data[i]))
+        {
+            chyba = CHYBA_ALOKACE;
+            return nova_matice;
+        }
+    }
+
+    // naplneni matice 
+    nova_matice.m = m;
+    nova_matice.n = n;
+    nova_matice.data = data;
 
     chyba = BEZ_CHYBY;
     return nova_matice;
-};
+}
 
 // funkce vytvorı jednotkovou matici typu m × n
-matice* jednotkova(int m, int n) {
-    if (m <= 0 || n <= 0) {
+matice jednotkova(int m, int n) {
+    matice nova_matice;
+
+    // kontrola nulove a zaporne velikosti matice
+    if (m <= 0 || n <= 0)
+    {
         chyba = CHYBA_TYPU;
-        return NULL;
+        return nova_matice;
     }
 
-    matice* nova_matice = inicializace(m, n);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                nova_matice->data[i][j] = 1;
-            } else {
-                nova_matice->data[i][j] = 0;
+    nova_matice = inicializace(m, n);
+    if (chyba != BEZ_CHYBY) {
+        return nova_matice;
+    }
+
+    // naplneni matice
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                nova_matice.data[i][j] = 1;
+            }
+            else
+            {
+                nova_matice.data[i][j] = 0;
             }
         }
     }
+
     chyba = BEZ_CHYBY;
     return nova_matice;
-};
+}
 
 // funkce vytvorı nulovou matici typu m × n
-matice* nulova(int m, int n) {
-    if (m <= 0 || n <= 0) {
+matice nulova(int m, int n) {
+    matice nova_matice;
+
+    // kontrola nulove a zaporne velikosti matice
+    if (m <= 0 || n <= 0)
+    {
         chyba = CHYBA_TYPU;
-        return NULL;
+        return nova_matice;
     }
 
-    matice* nova_matice = inicializace(m, n);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            nova_matice->data[i][j] = 0;
+    nova_matice = inicializace(m, n);
+    if (chyba != BEZ_CHYBY)
+    {
+        return nova_matice;
+    }
+
+    // naplneni matice
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            nova_matice.data[i][j] = 0;
         }
     }
+
     chyba = BEZ_CHYBY;
     return nova_matice;
-};
-
-// funkce, ktera dealokuje pamet alokovanou pro matici mat
-void odstran(matice* mat) {
-    if (!mat) {
-        chyba = CHYBA_JINA;
-        return;
-    }
-
-    for (int i = 0; i < mat->m; i++)
-    {
-        free(mat->data[i]);
-    }
-    free(mat->data);
-    free(mat);
-    chyba = BEZ_CHYBY;
-};
+}
 
 // funkce pro vypis matice do konzole
-void vypis(matice* mat) {
-    if (!mat) {
+void vypis(matice mat) {
+    // kontrola matice
+    if (!mat.data)
+    {
         chyba = CHYBA_JINA;
         return;
     }
 
-    for (int i = 0; i < mat->m; i++)
+    for (int i = 0; i < mat.m; i++)
     {
-        for (int j = 0; j < mat->n; j++)
+        for (int j = 0; j < mat.n; j++)
         {
-            printf("%f ", mat->data[i][j]);
+            printf("%f ", mat.data[i][j]);
         }
         printf("\n");
     }
-    chyba = BEZ_CHYBY;
-};
 
-// funkce, ktera nastavı prvek matice mat v i-tem radku a j-tem sloupci na hodnotu hodnota
-void nastav_prvek(matice* mat, int i, int j, float hodnota) {
-    if (!mat) {
+    chyba = BEZ_CHYBY;
+}
+
+// funkce, ktera dealokuje pamet alokovanou pro matici mat
+void odstran(matice mat) {
+    if (!mat.data)
+    {
         chyba = CHYBA_JINA;
         return;
     }
 
-    if (!(0 <= i <= mat->m) || !(0 <= j <= mat->n)) {
+    for (int i = 0; i < mat.m; i++)
+    {
+        free(mat.data[i]);
+    }
+    free(mat.data);
+    chyba = BEZ_CHYBY;
+}
+
+// funkce, ktera nastavı prvek matice mat v i-tem radku a j-tem sloupci na hodnotu hodnota
+void nastav_prvek(matice mat, int i, int j, float hodnota) {
+    if (!mat.data)
+    {
+        chyba = CHYBA_JINA;
+        return;
+    }
+
+    if (i < 0 || i > mat.m) {
         chyba = CHYBA_TYPU;
+        return;
+    }
+
+    if (j < 0 || j > mat.n) {
+        chyba = CHYBA_TYPU;
+        return;
     }
 
     if (!hodnota) {
-        chyba = CHYBA_TYPU;
+        chyba = CHYBA_JINA;
         return;
     }
 
-    mat->data[i][j] = hodnota;
+    mat.data[i][j] = hodnota;
     chyba = BEZ_CHYBY;
-};
+}
 
 // funkce, ktera vracı matici, ktera vznikla transpozicı matice mat
-matice* transpozice(matice* mat) {
-    if (!mat) {
+matice transpozice(matice mat) {
+    matice trans_matice;
+
+    if (!mat.data)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return trans_matice;
     }
 
-    matice* trans_matice = inicializace(mat->n, mat->m);
-    for (int i = 0; i < mat->m; i++) {
-        for (int j = 0; j < mat->n; j++) {
-            trans_matice->data[j][i] = mat->data[i][j];
+    trans_matice = inicializace(mat.n, mat.m);
+
+    for (int i = 0; i < mat.m; i++)
+    {
+        for (int j = 0; j < mat.n; j++) {
+            trans_matice.data[j][i] = mat.data[i][j];
         }
     }
+
     chyba = BEZ_CHYBY;
     return trans_matice;
-};
+}
 
 // funkce, ktera vracı soucet matic mat1 a mat2
-matice* plus(matice* mat1, matice* mat2) {
-    if (!mat1 || !mat2) {
+matice plus(matice mat1, matice mat2) {
+    matice sectena_matice;
+
+    if (!mat1.data || !mat2.data)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return sectena_matice;
     }
 
-    matice* sectena_matice = inicializace(mat1->m, mat1->n);
-    for (int i = 0; i < mat1->m; i++) {
-        for (int j = 0; j < mat1->n; j++) {
-            sectena_matice->data[i][j] = mat1->data[i][j] + mat2->data[i][j];
+    sectena_matice = inicializace(mat1.m, mat1.n);
+    if (chyba != BEZ_CHYBY)
+    {
+        return sectena_matice;
+    }
+
+    for (int i = 0; i < mat1.m; i++) {
+        for (int j = 0; j < mat1.n; j++) {
+            sectena_matice.data[i][j] = mat1.data[i][j] + mat2.data[i][j];
         }
     }
     chyba = BEZ_CHYBY;
     return sectena_matice;
-};
+}
 
 // funkce, ktera vracı rozdıl matic mat1 a mat2
-matice* minus(matice* mat1, matice* mat2) {
-    if (!mat1 || !mat2) {
+matice minus(matice mat1, matice mat2) {
+    matice odectena_matice;
+
+    if (!mat1.data || !mat2.data)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return odectena_matice;
     }
 
-    matice* odectena_matice = inicializace(mat1->m, mat1->n);
-    for (int i = 0; i < mat1->m; i++) {
-        for (int j = 0; j < mat1->n; j++) {
-            odectena_matice->data[i][j] = mat1->data[i][j] - mat2->data[i][j];
+    odectena_matice = inicializace(mat1.m, mat1.n);
+    if (chyba != BEZ_CHYBY)
+    {
+        return odectena_matice;
+    }
+
+    for (int i = 0; i < mat1.m; i++) {
+        for (int j = 0; j < mat1.n; j++) {
+            odectena_matice.data[i][j] = mat1.data[i][j] - mat2.data[i][j];
         }
     }
     chyba = BEZ_CHYBY;
     return odectena_matice;
-};
+}
 
 // funkce, ktera vracı matici, ktera vznikla z matice mat vynasobenım skalarem skalar
-matice* nasobeni(matice* mat, float skalar) {
-    if (!mat || !skalar) {
-        chyba = CHYBA_JINA;
-        return NULL;
+matice nasobeni(matice mat, float skalar) {
+    matice vynasobena_matice = inicializace(mat.m, mat.n);
+    if (chyba != BEZ_CHYBY)
+    {
+        return vynasobena_matice;
     }
 
-    matice* vynasobena_matice = inicializace(mat->m, mat->n);
-    for (int i = 0; i < mat->m; i++) {
-        for (int j = 0; j < mat->n; j++) {
-            vynasobena_matice->data[i][j] = mat->data[i][j] * skalar;
+    if (!mat.data || !skalar)
+    {
+        chyba = CHYBA_JINA;
+        return vynasobena_matice;
+    }
+
+    for (int i = 0; i < mat.m; i++) {
+        for (int j = 0; j < mat.n; j++) {
+            vynasobena_matice.data[i][j] = mat.data[i][j] * skalar;
         }
     }
     chyba = BEZ_CHYBY;
     return vynasobena_matice;
-};
+}
 
 // funkce, ktera vracı soucin matic mat1 a mat2
-matice* krat(matice* mat1, matice* mat2) {
-    if (!mat1 || !mat2) {
+matice krat(matice mat1, matice mat2) {
+    matice vynasobena_matice = inicializace(mat1.m, mat2.n);
+    if (chyba != BEZ_CHYBY)
+    {
+        return vynasobena_matice;
+    }
+
+    if (!mat1.data || !mat2.data)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return vynasobena_matice;
     }
 
-    if (mat1->m == mat2->m || mat1->n == mat2->n) {
+    if (mat1.m == mat2.m || mat1.n == mat2.n) {
         chyba = CHYBA_TYPU;
-        return NULL;
+        return vynasobena_matice;
     }
 
-    matice* vynasobena_matice = inicializace(mat1->m, mat2->n);
-    for (int i = 0; i < mat1->m; i++) {
-        for (int j = 0; j < mat2->n; j++) {
-            for (int k = 0; k < mat1->n; k++)
+    for (int i = 0; i < mat1.m; i++) {
+        for (int j = 0; j < mat2.n; j++) {
+            for (int k = 0; k < mat1.n; k++)
             {
-                vynasobena_matice->data[i][j] += mat1->data[i][k] * mat2->data[k][j];
+                vynasobena_matice.data[i][j] += mat1.data[i][k] * mat2.data[k][j];
             }
         }
     }
     chyba = BEZ_CHYBY;
     return vynasobena_matice;
-};
+}
 
 // funkce, ktera vracı velikost matice v dimenzi dimenze
-int velikost(matice* mat, int dimenze) {
-    if (!mat) {
+int velikost(matice mat, int dimenze) {
+    if (!mat.data) {
         chyba = CHYBA_JINA;
         return -1;
     }
 
     if (dimenze == 0) {
-        return mat->m;
+        return mat.m;
     } else if (dimenze == 1) {
-        return mat->n;
+        return mat.n;
     } else {
         chyba = CHYBA_JINA;
         return -1;
     }
-};
+}
 
 // funkce, ktera vracı prvek matice mat v i-tem radku a j-tem sloupci
-float prvek(matice* mat, int i, int j) {
-    if (!mat) {
+float prvek(matice mat, int i, int j) {
+    if (!mat.data)
+    {
         chyba = CHYBA_JINA;
         return -1;
     }
 
-    if (!(0 <= i <= mat->m) || !(0 <= j <= mat->n)) {
+    if (i < 0 || i > mat.m)
+    {
         chyba = CHYBA_TYPU;
         return -1;
     }
 
-    return mat->data[i][j];
-};
+    if (j < 0 || j > mat.n)
+    {
+        chyba = CHYBA_TYPU;
+        return -1;
+    }
+
+    return mat.data[i][j];
+}
 
 // funkce, ktera vracı matici, ktera je ulozena v souboru soubor. Kazdy radek souboru reprezentuje radek matice a jednotlive prvky matice jsou oddeleny mezerou.
-matice* nacti_ze_souboru(const char *soubor) {
-    if (!soubor) {
+matice nacti_ze_souboru(const char *soubor) {
+    matice nova_matice;
+
+    if (!soubor)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return nova_matice;
     }
 
     FILE *otevreny_soubor;
     otevreny_soubor = fopen(soubor,"r");
     if (otevreny_soubor == NULL || !otevreny_soubor) {
         chyba = CHYBA_OTEVRENI;
-        return NULL;
+        return nova_matice;
     }
 
     int rows = 0;
@@ -295,43 +385,48 @@ matice* nacti_ze_souboru(const char *soubor) {
 
     cols = cols/rows;
 
-    matice* nova_matice = inicializace(rows, cols);
+    nova_matice = inicializace(rows, cols);
+    if (chyba != BEZ_CHYBY)
+    {
+        return nova_matice;
+    }
 
     rewind(otevreny_soubor);
 
     for (int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             fscanf (otevreny_soubor, "%f", &f);
-            nova_matice->data[i][j] = f;
+            nova_matice.data[i][j] = f;
         }
     }
 
     if (!(fclose(otevreny_soubor) == 0)) {
         chyba = CHYBA_ZAVRENI;
-        return NULL;
+        return nova_matice;
     }
 
     chyba = BEZ_CHYBY;
     return nova_matice;
-};
+}
 
 // funkce, ktera ulozı matici mat do souboru soubor (ve stejnem formatu, jako v predchozım bodu)
-matice* uloz_do_souboru(matice* mat, const char *soubor) {
-    if (!soubor) {
+matice uloz_do_souboru(matice mat, const char *soubor) {
+    if (!soubor)
+    {
         chyba = CHYBA_JINA;
-        return NULL;
+        return mat;
     }
 
     FILE *otevreny_soubor;
     otevreny_soubor = fopen(soubor,"w");
     if (otevreny_soubor == NULL) {
         chyba = CHYBA_OTEVRENI;
-        return NULL;
+        return mat;
     }
 
-    for (int i = 0; i < mat->m; i++) {
-        for (int j = 0; j < mat->n; j++) {
-            fprintf(otevreny_soubor,"%f",mat->data[i][j]);
+    for (int i = 0; i < mat.m; i++) {
+        for (int j = 0; j < mat.n; j++) {
+            fprintf(otevreny_soubor,"%f",mat.data[i][j]);
             fprintf(otevreny_soubor,"%s"," ");
         }
         fprintf(otevreny_soubor,"%s","\n");
@@ -339,33 +434,9 @@ matice* uloz_do_souboru(matice* mat, const char *soubor) {
 
     if (!(fclose(otevreny_soubor) == 0)) {
         chyba = CHYBA_ZAVRENI;
-        return NULL;
+        return mat;
     }
 
     chyba = BEZ_CHYBY;
     return mat;
-};
-
-int kontrolaChyb() {
-    switch (chyba) {
-        case CHYBA_TYPU:
-            printf("Chyba pri provadenı operacı s nespravnou velikostı matice.");
-            return 1;
-        case CHYBA_ALOKACE:
-            printf("Nastala chyba alokace pameti.");
-            return 1;
-        case CHYBA_OTEVRENI:
-            printf("Nastala chyba pri otevıranı souboru.");
-            return 1;
-        case CHYBA_ZAVRENI:
-            printf("Nastala chyba pri zavirani souboru.");
-            return 1;
-        case CHYBA_JINA:
-            printf("Nastala jina chyba.");
-            return 1;
-        case BEZ_CHYBY:
-            break;
-    }
-
-    return 0;
 }
